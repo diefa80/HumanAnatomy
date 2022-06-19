4cee6de05ce38be54ba2c703c6a441b807c7ab0d
Dinka Efa Gemeda <78479372+diefa80@users.noreply.github.com>
2022-06-19 11:33:33 +0300

    commit

---
+++ humananatomy/TextureLoader.py
@@ -0,0 +1,45 @@
+from OpenGL.GL import glBindTexture, glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, \
+    GL_TEXTURE_WRAP_T, GL_REPEAT, GL_TEXTURE_MIN_FILTER, GL_TEXTURE_MAG_FILTER, GL_LINEAR,\
+    glTexImage2D, GL_RGBA, GL_UNSIGNED_BYTE
+
+from PIL import Image
+
+
+# using GLFW
+def load_texture(path, texture):
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # texture wraping parameter
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Setting texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # loading image here
+    image = Image.open(path)
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = image.convert("RGBA").tobytes()
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return texture
+
+
+# using
+
+
+
++ pygame
+def load_texture_pygame(path, texture):
+    import pygame
+    glBindTexture(GL_TEXTURE_2D, texture)
+    # Setting the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    # Setting texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # load image 
+    image = pygame.image.load(path)
+    image = pygame.transform.flip(image, False, True)
+    image_width, image_height = image.get_rect().size
+    img_data = pygame.image.tostring(image, "RGBA")
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return texture
